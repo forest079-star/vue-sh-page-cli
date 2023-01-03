1a1a4f63750f09ab7efa28c66017984ec4016d62
forest079 <bluegx08@gmail.com>
2023-01-03 17:30:43 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' 
+  ? '/vue-sh-page-cli/'  // 資料夾路徑，儲存庫名稱
+  : '/',
+});
